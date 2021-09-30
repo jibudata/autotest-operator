4a8310480899fcd2989a91fc1760d8b4aa1417bf
hpjibu <88641193+hpjibu@users.noreply.github.com>
2021-09-30 16:33:57 +0800

    Update install_app.sh

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -1,5 +1,5 @@
 kubectl create namespace application
-kubectl -n application apply -f ./autotest-operator/config/mysql-deployment.yaml
+kubectl -n application apply -f /root/autotest-operator/config/mysql-deployment.yaml
 sleep 20
-kubectl -n application apply -f ./autotest-operator/config/autotest.yaml
-kubectl -n application apply -f ./autotest-operator/config/nginx-deployment.yaml
+kubectl -n application apply -f /root/autotest-operator/config/autotest.yaml
+kubectl -n application apply -f /root/autotest-operator/config/nginx-deployment.yaml
